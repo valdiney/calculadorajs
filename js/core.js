a0eb24c329311f658dc2ea6e721c59e7bf0c712f
Valdiney França <valdiney.2@hotmail.com>
2013-07-25 12:42:32 -0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,161 +1,208 @@
-/////////////////////////////////////
+///////////////////////////////////////////////////////
 // Projeto Calculadora JS
-// Autor: Valdiney França
+// Autor: Valdiney FranÃ§a
 // Data: 21/07/2013
-////////////////////////////////////
+//////////////////////////////////////////////////////
 
-window.onload = function(){
+window.onload = function() {
 
 //////////////////////////////////////////////////////
-// Funções inicias... Evento onclick nos botões
+// FunÃ§Ãµes inicias... Evento onclick nos botÃµes
 /////////////////////////////////////////////////////
-var sete = document.getElementById('sete').onclick = function(){
- calculando(7);
-}// end sete
-var oito = document.getElementById('oito').onclick = function(){
- calculando(8);
-}// end oito
-var nove = document.getElementById('nove').onclick = function(){
- calculando(9);
-}// end nove
-var quatro = document.getElementById('quatro').onclick = function(){
- calculando(4);
-}// end quatro
-var cinco = document.getElementById('cinco').onclick = function(){
- calculando(5);
-}// end cinco
-var seis = document.getElementById('seis').onclick = function(){
- calculando(6);
-}// end seis
-var um = document.getElementById('um').onclick = function(){
- calculando(1);
-}// end um
-var dois = document.getElementById('dois').onclick = function(){
- calculando(2);
-}// end dois
-var tres = document.getElementById('tres').onclick = function(){
- calculando(3);
-}// end tres
-var zero = document.getElementById('zero').onclick = function(){
- calculando(0);
-}// end zero
-// end..............................
+
+
+var sete = document.getElementById('sete').onclick = function() {
+  calculando(7);
+} // end sete
+
+var oito = document.getElementById('oito').onclick = function() {
+  calculando(8);
+} // end oito
+
+var nove = document.getElementById('nove').onclick = function() {
+  calculando(9);
+} // end nove
+
+var quatro = document.getElementById('quatro').onclick = function() {
+  calculando(4);
+} // end quatro
+
+var cinco = document.getElementById('cinco').onclick = function() {
+  calculando(5);
+} // end cinco
+
+var seis = document.getElementById('seis').onclick = function() {
+  calculando(6);
+} // end seis
+
+var um = document.getElementById('um').onclick = function() {
+  calculando(1);
+} // end um
+
+var dois = document.getElementById('dois').onclick = function() {
+  calculando(2);
+} // end dois
+
+var tres = document.getElementById('tres').onclick = function() {
+  calculando(3);
+} // end tres
+
+var zero = document.getElementById('zero').onclick = function() {
+  calculando(0);
+} // end zero
+/////////////////////////////////////////////////////
+
+
 /////////////////////////////////////////////////////////////
-// Evento onclick nos Botões que fazem o papel de operadores
+// Evento onclick nos BotÃµes que fazem o papel de operadores
 ////////////////////////////////////////////////////////////
-var ponto = document.getElementById('ponto').onclick = function(){
- if(document.getElementById('visor').value == ''){
- document.getElementById('visor').value = '';
- } else {
- calculando('.');
+var ponto = document.getElementById('ponto').onclick = function() {
+var visor = document.getElementById('visor').value;
+ if(visor == '') {
+   document.getElementById('visor').value = '';
+ } 
+ else {
+   calculando('.');
  }
 }// end ponto
-var soma = document.getElementById('operador_mais').onclick = function(){
+
+var soma = document.getElementById('operador_mais').onclick = function() {
 var visor = document.getElementById('visor').value;
- if( visor == ''){
- visor = '';
- } else {
- calculando('+');
+ if(visor == '') {
+   visor = '';
+ } 
+ else {
+   calculando('+');
  }
 }// end soma
-var subtraindo = document.getElementById('operador_diminuir').onclick = function(){
+
+var subtraindo = document.getElementById('operador_diminuir').onclick = function() {
 var visor = document.getElementById('visor').value;
- if( visor == ''){
- visor = '';
- } else {
- calculando('-');
+ if(visor == '') {
+   visor = '';
+ } 
+ else {
+   calculando('-');
  }
 }// end subtrair
-var multiplicando = document.getElementById('operador_multiplicar').onclick = function(){
+
+var multiplicando = document.getElementById('operador_multiplicar').onclick = function() {
 var visor = document.getElementById('visor').value;
- if( visor == ''){
-  visor = '';
- } else {
- calculando('*');
+ if(visor == '') {
+   visor = '';
+ } 
+ else {
+  calculando('*');
  }
-}// end multiplicação
-var dividindo = document.getElementById('operador_divisao').onclick = function(){
+}// end multiplicaÃ§Ã£o
+
+var dividindo = document.getElementById('operador_divisao').onclick = function() {
 var visor = document.getElementById('visor').value;
- if( visor== ''){
- visor = '';
- } else {
- calculando('/');
+ if(visor == '') {
+   visor = '';
+ } 
+ else {
+   calculando('/');
  }
-}// end divisão
-// end operadores................................
-/////////////////////////////
-// Incluindo os Parênteses
-////////////////////////////
-var parentese_left = document.getElementById('parentese_left').onclick = function(){
- calculando('(');
+}// end divisÃ£o
+//////////////////////////////////////////////////////////// 
+
+
+//////////////////////////////////
+// Incluindo os ParÃªnteses
+/////////////////////////////////
+var parentese_left = document.getElementById('parentese_left').onclick = function() {
+  calculando('(');
 }
-var parentese_right = document.getElementById('parentese_right').onclick = function(){
+var parentese_right = document.getElementById('parentese_right').onclick = function() {
  calculando(')');
 }
-////////////////////
+///////////////////////////////////
+
+
+//////////////////////////////////
 // Raiz quadrada...
-///////////////////
-var raiz_quadrada = document.getElementById('raiz_quadrada').onclick = function(){
+/////////////////////////////////
+var raiz_quadrada = document.getElementById('raiz_quadrada').onclick = function() {
 var visor = document.getElementById('visor').value;
- if( visor == ''){
- visor= '';
- } else {
- calculando(" = Raiz "+Math.sqrt(document.getElementById('visor').value));
+ if(visor == '') {
+   visor= '';
+ } 
+ else {
+   calculando(' = Raiz '+Math.sqrt(document.getElementById('visor').value));
  }
 }
-////////////////////
+
+
+//////////////////////////////
 // Porcentagem...
-///////////////////
-var porcentagem = document.getElementById('porcentagem').onclick = function(){ 
+/////////////////////////////
+var porcentagem = document.getElementById('porcentagem').onclick = function() { 
 var visor = document.getElementById('visor').value; 
- if( visor == ''){
- visor = '';
- } else {
- calculando('/100');
+ if(visor == '') {
+   visor = '';
+ } 
+ else {
+  calculando('/100');
  }
 }
-////////////////////////////
+
+
+///////////////////////////////////
 // Calculando o valor de PI
-///////////////////////////
-var constante_pi = document.getElementById('pi').onclick = function(){ 
+//////////////////////////////////
+var constante_pi = document.getElementById('pi').onclick = function() { 
 var visor = document.getElementById('visor').value; 
- if( visor == ''){
- visor = '';
- } else {
- calculando(" PI = "+2*Math.PI*document.getElementById('visor').value);
+ if(visor == '') {
+   visor = '';
+ } 
+ else {
+   calculando(' PI = '+2*Math.PI*document.getElementById('visor').value);
  }
 }
-// end ...............................
-/////////////////////////////
-// Ordenando os números na tela, ou seja, visor.
-/////////////////////////////
-function calculando(numeros){
+///////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////
+// Ordenando os nÃºmeros na tela, ou seja, visor.
+/////////////////////////////////////////////////
+function calculando(numeros) {
  document.getElementById('visor').value = document.getElementById('visor').value + numeros;
 }
 ////////////////////////////////////////////
-// Função 'igual' faz os calculos dos valores.
-///////////////////////////////////////////
-/*Relatório*/ var relatorio_resultado = document.getElementById('b_resultados');
 
-var igual = document.getElementById('igual').onclick = function(){
+
+////////////////////////////////////////////
+// FunÃ§Ã£o 'igual' faz os calculos dos valores.
+///////////////////////////////////////////
+
+var relatorio_resultado = document.getElementById('b_resultados');
+///////////////////////////////////////////
+
+var igual = document.getElementById('igual').onclick = function() {
 var visor = document.getElementById('visor').value;
  document.getElementById('erros');// Recupera id 'erros'.
- if( visor == ''){
-  erros.innerHTML = "Digite números e operadores antes de computar. Exemplo: 5+3";
- } else {
- relatorio_resultado.innerHTML += visor+" = ";
- /* Calculo */ document.getElementById('visor').value = eval(document.getElementById('visor').value);
- relatorio_resultado.innerHTML +=  eval(document.getElementById('visor').value)+", ";
- erros.innerHTML = "Calculadora (JS): Por Valdiney França";
+ if(visor == '') {
+   erros.innerHTML = "Digite nÃºmeros e operadores antes de computar. Exemplo: 5+3";
+ } 
+ else {
+  relatorio_resultado.innerHTML += visor+' = ';
+  
+  document.getElementById('visor').value = eval(document.getElementById('visor').value);
+  /////////////////////////////////////////////
+  relatorio_resultado.innerHTML +=  eval(document.getElementById('visor').value)+', ';
+  erros.innerHTML = 'Calculadora (JS): Por Valdiney FranÃ§a';
  }
 }// end igual
+
+
 /////////////////////////////////////
 // Limpa o campo de valores 'visor'.
 ////////////////////////////////////
 var limpar_visor = document.getElementById('limpar').onclick = function(){
- document.getElementById('visor').value = '';
+  document.getElementById('visor').value = '';
 }// end limpar
 
 
 
-} //....end window...........
+} // end window
